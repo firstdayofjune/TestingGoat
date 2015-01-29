87d70e0c299f3a9693d5817bd0535ef2038115d3
firstdayofjune <firstdayofjune@users.noreply.github.com>
2015-01-29 22:22:03 +0100

    Changed functional test to use unittest-module, added some descriptive comments

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Opera()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Julius has heard about a really nice and new TO-DO list, so he heads to the url and expects to find the word "To-Do" in the browsers title
+		self.browser.get('http://localhost:8000')
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+		
+		# After Julius has had a productive session putting all the things he has to do in an order, he quits his browser, shuts his PC down and gets some ice-cream.
+		# tearDonw-method will quit the browser
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
