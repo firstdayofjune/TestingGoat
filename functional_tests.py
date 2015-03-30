9e594febe728c2b4ae79bb6b4213a7bdd8ca9eb7
firstdayofjune <firstdayofjune@users.noreply.github.com>
2015-03-30 20:45:55 +0200

    Refactored the functional test

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Julius has heard about a really nice and new TO-DO list, so he heads to the url and expects to find the word "To-Do" in the browsers title
 		self.browser.get('http://localhost:8000')
@@ -36,11 +41,9 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)		
 
-		# Now the recently added to-do item is shown in the to-do list table
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		# The page updates and the two to do items are displayed in an enumerated list
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# There now still is a input-box, where new to-do items can be added
 		self.fail('Finish the test!')
